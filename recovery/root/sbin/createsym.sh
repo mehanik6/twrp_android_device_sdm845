bc4c4ee47b19ae51e1a5ce6ff41dec7a89f523d9
mehanik6 <mehanik67@bk.ru>
2018-09-20 21:09:15 +0300

    fix_data2

---
+++ recovery/root/sbin/createsym.sh
@@ -0,0 +1,23 @@
+#!/sbin/sh
+# workaround script by steadfasterX to ensure we have the required symlink on bootdevice
+
+LOG=/tmp/recovery.log
+F_LOG(){
+   MSG="$1"
+   echo "I:$TAG: $(date +%F_%T) - $MSG" >> $LOG
+}
+F_ELOG(){
+   MSG="$1"
+   echo "E:$TAG: $(date +%F_%T) - $MSG" >> $LOG
+}
+
+TAG="SYMLINK"
+
+F_LOG "Starting $0"
+F_LOG "bootdevice check before actually doing anything: >$(ls -la /dev/block/ |grep bootdevice)<"
+rm -Rf /dev/block/bootdevice >> $LOG
+LNKERR=0
+[ -d /dev/block/platform/soc/1d84000.ufshc/ ] && ln -vs /dev/block/platform/soc/1d84000.ufshc /dev/block/bootdevice >> $LOG
+LNKERR=$((LINKERR + $?))
+F_LOG "bootdevice check after fixing the symlink: >$(ls -la /dev/block/ |grep bootdevice)<"
+F_LOG "$0 finished with <$LNKERR>"
--- recovery/root/sbin/prepdecrypt.sh
+++ recovery/root/sbin/prepdecrypt.sh
@@ -1,14 +1,103 @@
 #!/sbin/sh
 
+LOG=/tmp/recovery.log
+TAG=PREPDEC
+
+F_LOG(){
+   MSG="$1"
+   echo -e "I:$TAG: $(date +%F_%T) - $MSG" >> $LOG
+}
+F_ELOG(){
+   MSG="$1"
+   echo -e "E:$TAG: $(date +%F_%T) - $MSG" >> $LOG
+}
+F_LOG "Started $0"
+
 relink()
 {
-	/sbin/cp $1 ${1}_old
-	sed 's|/system/bin/linker64|///////sbin/linker64|' "${1}_old" > "$1"
-	chmod 755 $1
+	fname=$(basename "$1")
+	target="/sbin/$fname"
+	sed 's|/system/bin/linker64|///////sbin/linker64|' "$1" > "$target"
+	chmod 755 $target
 }
 
-relink /sbin/qseecomd
-relink /sbin/android.hardware.gatekeeper@1.0-service-qti
-relink /sbin/android.hardware.keymaster@3.0-service-qti
+# the dev path can be different so we need to identify it
+syspathsoc="/dev/block/platform/soc/1.d84000.ufshc/by-name/system"
+syspathnosoc="/dev/block/platform/1.d84000.ufshc/by-name/system"
+syspath=undefined
+while [ ! -e "$syspath" ];do
+    [ -e "$syspathnosoc" ] && syspath="$syspathnosoc"
+    [ -e "$syspathsoc" ] && syspath="$syspathsoc"
+    F_LOG "syspath: $syspath"
+    [ "$syspath" == "undefined" ] && F_LOG "sleeping a bit as syspath is not there yet.." && sleep 1
+done
 
-/sbin/setprop crypto.ready 1
+# prepare and mount
+mkdir /s >> $LOG 2>&1 
+mount -t ext4 -o ro "$syspath" /s  >> $LOG 2>&1 || F_ELOG "mounting /s to $syspath failed"
+
+# directories
+F_LOG "$(echo "Preparing directories:"; \
+mkdir /vendor 2>&1 ; \ 
+mkdir -p /system/etc 2>&1 ; \
+mkdir -p /vendor/lib64/hw/ 2>&1 ; \
+mkdir /persist-lg 2>&1 ; \ 
+mkdir /firmware 2>&1)"
+
+# this relinks (linker64) AND copies qseecomd to /sbin
+if [ -f /s/vendor/bin/qseecomd ];then
+    relink /s/vendor/bin/qseecomd  >> $LOG 2>&1
+    [ $? -ne 0 ] && F_ELOG "relinking qseecomd failed (vendor)"
+else
+    relink /s/bin/qseecomd >> $LOG 2>&1 || F_ELOG "relinking qseecomd failed"
+    [ $? -ne 0 ] && F_ELOG "relinking qseecomd failed (system)"
+fi
+
+F_LOG "preparing libraries..."
+
+# copy the hws stuff
+cp /s/bin/hwservicemanager /sbin/ >> $LOG 2>&1 
+cp /s/lib64/libandroid_runtime.so /sbin/ >> $LOG 2>&1 
+cp /s/lib64/libhidltransport.so /sbin/ >> $LOG 2>&1 
+cp /s/lib64/libhidlbase.so /sbin/ >> $LOG 2>&1 
+
+# copy the decrypt stuff
+cp /s/vendor/lib64/libdiag.so /sbin/ >> $LOG 2>&1 
+cp /s/vendor/lib64/libdrmfs.so /sbin/ >> $LOG 2>&1 
+cp /s/vendor/lib64/libdrmtime.so /sbin/ >> $LOG 2>&1 
+cp /s/vendor/lib64/libQSEEComAPI.so /sbin/ >> $LOG 2>&1 
+cp /s/vendor/lib64/librpmb.so /sbin/ >> $LOG 2>&1 
+cp /s/vendor/lib64/libssd.so /sbin/ >> $LOG 2>&1 
+cp /s/vendor/lib64/libtime_genoff.so /sbin/ >> $LOG 2>&1 
+#cp /s/vendor/manifest.xml /vendor/ >> $LOG 2>&1 
+#cp /s/vendor/compatibility_matrix.xml /vendor/ >> $LOG 2>&1 
+cp /s/vendor/lib64/hw/android.hardware.keymaster@3.0-impl-qti.so /sbin/android.hardware.keymaster@3.0-impl-qti.so >> $LOG 2>&1 
+cp /s/vendor/lib64/hw/android.hardware.gatekeeper@1.0-impl-qti.so /sbin/android.hardware.gatekeeper@1.0-impl-qti.so >> $LOG 2>&1 
+
+cp /s/vendor/lib64/hw/android.hardware.gatekeeper@1.0-impl-qti.so /vendor/lib64/hw/android.hardware.gatekeeper@1.0-impl-qti.so >> $LOG 2>&1 
+cp /s/vendor/lib64/hw/android.hardware.keymaster@3.0-impl-qti.so /vendor/lib64/hw/android.hardware.keymaster@3.0-impl-qti.so >> $LOG 2>&1 
+cp /s/vendor/lib64/libmdtp.so /vendor/lib64/libmdtp.so >> $LOG 2>&1 
+cp /s/vendor/lib64/libqmi_common_so.so /vendor/lib64/libqmi_common_so.so >> $LOG 2>&1 
+cp /s/vendor/lib64/libsmemlog.so /vendor/lib64/libsmemlog.so >> $LOG 2>&1 
+cp /s/vendor/lib64/libqmiservices.so /vendor/lib64/libqmiservices.so >> $LOG 2>&1 
+cp /s/vendor/lib64/libqmi_encdec.so /vendor/lib64/libqmi_encdec.so >> $LOG 2>&1 
+cp /s/vendor/lib64/libqmi_client_qmux.so /vendor/lib64/libqmi_client_qmux.so >> $LOG 2>&1 
+cp /s/vendor/lib64/libqmi_cci.so /vendor/lib64/libqmi_cci.so >> $LOG 2>&1 
+cp /s/vendor/lib64/libmdmdetect.so /vendor/lib64/libmdmdetect.so >> $LOG 2>&1 
+cp /s/vendor/lib64/libidl.so /vendor/lib64/libidl.so >> $LOG 2>&1 
+cp /s/vendor/lib64/libdsutils.so /vendor/lib64/libdsutils.so >> $LOG 2>&1 
+cp /s/vendor/lib64/libdiag.so /vendor/lib64/libdiag.so >> $LOG 2>&1 
+cp /s/vendor/lib64/libQSEEComAPI.so /vendor/lib64/libQSEEComAPI.so >> $LOG 2>&1 
+
+F_LOG "preparing libraries finished"
+
+umount /s >> $LOG 2>&1 || F_ELOG "unmounting /s failed"
+
+# inform init to start qseecomd
+setprop crypto.ready 1  >> $LOG 2>&1 
+F_LOG "crypto.ready: $(getprop crypto.ready)"
+
+F_LOG "current mounts: \n$(mount)"
+
+F_LOG "$0 ended"
+exit 0
